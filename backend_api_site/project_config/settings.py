08d1706c51a038876d6a37651f79a934ee53a050
Sebastian B. Fredriksson <sebefre@gmail.com>
2017-09-26 22:32:50 +0200

    Renamed config folder to project_config
    Felt like the name of the config folder wasn't clear enough so renamed
    it to project_config. Hopefully finished with renaming the config folder
    now.

--- backend_api_site/project_config/settings.py
+++ backend_api_site/project_config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api_main.urls'
+ROOT_URLCONF = 'project_config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api_main.wsgi.application'
+WSGI_APPLICATION = 'project_config.wsgi.application'
 
 
 # Database

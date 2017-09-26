e78a824ba955bcfd15508dfab177ae825baaf3a5
Sebastian B. Fredriksson <sebefre@gmail.com>
2017-09-26 20:34:40 +0200

    Renamed main package to from backend_api to site

--- backend_api_site/site/settings.py
+++ backend_api_site/site/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend_api.urls'
+ROOT_URLCONF = 'site.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend_api.wsgi.application'
+WSGI_APPLICATION = 'site.wsgi.application'
 
 
 # Database

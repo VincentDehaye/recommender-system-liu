23049a427dfa9aaa9928f684708a19b24dcc1a27
Marten Bolin <marbo914@student.liu.se>
2017-09-28 16:34:47 +0200

    Added models to db Movie and Genre /marbo914 & johli715

--- Product/Database/DBFillRecommendations.py
+++ Product/Database/DBFillRecommendations.py
@@ -1,7 +1,8 @@
 from Product.Database.DBConn import session
 #!/bin/bash
 import csv
+
 with open('movies.csv', 'rt') as f:
     reader = csv.reader(f)
     for row in reader:
-        print(row) 
+        print(row)

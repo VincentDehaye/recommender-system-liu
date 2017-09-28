4d36122f0c637503f93dbe6d145c4918ae2d49f0
johli715 <johli715@student.liu.se>
2017-09-28 16:36:34 +0200

    Created Genre list for adding to db /johli715 and marbo914

--- Product/Database/DBFillRecommendations.py
+++ Product/Database/DBFillRecommendations.py
@@ -1,7 +1,15 @@
 from Product.Database.DBConn import session
 #!/bin/bash
 import csv
+
+genreList = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary",
+             "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi",
+             "Thriller", "War", "Western", "no genres listed"]
+
+for genre in genreList:
+    session.add(genre)
+
 with open('movies.csv', 'rt') as f:
     reader = csv.reader(f)
     for row in reader:
-        print(row) 
+        print(row)

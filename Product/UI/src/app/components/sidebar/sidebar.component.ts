987d1f0b29a1654f57ad197fd97f77f9101fe96a
Erik <erito014@student.liu.se>
2017-10-02 12:05:38 +0200

    added a sidebar

--- Product/UI/src/app/components/sidebar/sidebar.component.ts
+++ Product/UI/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: 'firstview', title: 'First View',  icon: 'pe-7s-graph', class: '' },
+    { path: 'secondview', title: 'Second View',  icon:'pe-7s-user', class: '' },
+    { path: 'thirdview', title: 'Third View',  icon:'pe-7s-note2', class: '' },
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
-
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
 }

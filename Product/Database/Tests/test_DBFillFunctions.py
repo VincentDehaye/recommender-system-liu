2f6f560b6e20ef1daef7ce0a01687ae4412bcf82
John Lidquist <johli715@student.liu.se>
2017-10-02 14:46:19 +0200

    Finished the README file (in Database folder) and commented database files where needed

--- Product/Database/Tests/test_DBFillFunctions.py
+++ Product/Database/Tests/test_DBFillFunctions.py
@@ -1,7 +1,13 @@
 from Product.Database.DBConn import User, Movie, Genre, MovieInGenre, Rating, MovieLinks
 from Product.Database.DBConn import session
 
+# This is the file to unit test the four DBFill files (DBFillUsers, DBFillMovies, DBFillRatings, DBFillLinks)
+# Before the test is run the db should have been created and the four files runned (you can do this by running
+# the "DBFillMovieLens" which will run the four files). This test file will make sure that the first and last
+# entries in the corresponding csv files has been successfully entered into the db. We check this by doing queries
+# and asserting the result is the expected
 
+# Tests for DBFillUsers
 def test_DBFillUsers():
     result = session.query(User).filter_by(id=1).first()
     assert result.id == 1
@@ -9,7 +15,7 @@ def test_DBFillUsers():
     result = session.query(User).filter_by(id=700).first()
     assert result.id == 700
 
-
+# Tests for DBFillMovies
 def test_DBFillMovies():
     result = session.query(Genre).filter_by(name='Action').first()
     assert result.name == 'Action'
@@ -38,7 +44,7 @@ def test_DBFillMovies():
     result = session.query(MovieInGenre).filter_by(movie_id=164979).first()
     assert result.genre == "Documentary"
 
-
+# Tests for DBFillRatings
 def test_DBFillRatings():
     result = session.query(Rating).filter_by(user_id=1, movie_id=31).first()
     assert result.rating == 2.5
@@ -46,7 +52,7 @@ def test_DBFillRatings():
     result = session.query(Rating).filter_by(user_id=671, movie_id=6565).first()
     assert result.rating == 3.5
 
-
+# Tests for DBFillLinks
 def test_DBFillLinks():
     result = session.query(MovieLinks).filter_by(movie_id=1).first()
     assert result.imdb_id == 114709

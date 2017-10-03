68638f21778f0ad2983ce186676329814164da2c
marlu642 <marlu642@student.liu.se>
2017-10-03 14:44:40 +0200

    Created a main method. Split functions into smaller functions.

--- Product/TrendManager/TrendingController.py
+++ Product/TrendManager/TrendingController.py
@@ -3,7 +3,7 @@
 # Purpose: Controller class for the trending module. Gets data, calculates a score
 # and sends it to the database API.
 
-# import YoutubeApi
+# from .YoutubeAPI import YoutubeAPI
 from .ScoredMovie import ScoredMovie
 
 # This is just a suggestion of how the TrendingController could look like.
@@ -13,11 +13,12 @@ from .ScoredMovie import ScoredMovie
 class TrendingController:
     scoredMovie = ScoredMovie()
 
-    # def __init__(self):
-        # self.youtubeData = YoutubeApi.getData("Keyword")
+    def __init__(self):
+        # self.youtubeData = YoutubeAPI.getData("Keyword")
         # scoredMovie.id = self.youtubeData[id]
         # scoredMovie.score = YoutubeScoreCalc(self.youtubeData[views], other...)
         # SendToDatabase(scoredMovie)
+        print ("placeholder")
 
     # def YoutubeScoreCalc(self, views, other...):
 

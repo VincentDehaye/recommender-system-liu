cf4d4d1d0a071b78d88aa9e5cba8f693bd4ae0f5
Alexander Dahl <aleda145@student.liu.se>
2017-10-03 16:40:00 +0200

    output to visualization is now a dictionary. The output is not finalized, needs further work when the correct dataset in the database is setup

--- Product/RecommendationManager/example/lightfm_example.py
+++ Product/RecommendationManager/example/lightfm_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import json as json
 from lightfm import LightFM
 # TODO Instead of getting the dataset from movielens, we should get it from our own DB.
 from lightfm.datasets import fetch_movielens
@@ -56,39 +56,41 @@ def sample_recommendation(model, data, user_ids):
 # That is because arrays start at 0 in python and.
 # TODO Each movie should be related to an id in our own database so that the integration with trending is done smootlhy.
 # TODO The output from this function should be a list of length 10 with ID:s that corresponds to the predicted movies.
-sample_recommendation(model, data, range(0, 3))
+#sample_recommendation(model, data, range(0, 3))
 
 
 
 # TODO find out what argsort is. read how numpy works.
 def sample_output_to_visualisation(model, data):
     n_users, n_items = data['train'].shape
+    dictionary = {}
 
     for user_id in range(0, n_users):
 
         scores = model.predict(user_id, np.arange(n_items))
-        print('score for user %s ' % user_id)
-        print(scores)
 
-        print(np.sort(-scores))
+        print('prints the top 10 scores user %s' % user_id)
+        print(np.sort(-scores[:10]).tolist())
+        # np.argsort(-scores) outputs the indexes for the movie ids (sorted by score)
+        # When we change the dataset we will instead output movie id.
+        # there will be a database call to get the right movie_id
 
-        print(np.argsort(-scores))
-        top_items = data['item_labels'][np.argsort(-scores)]
-        top_items_with_labels = data['item_labels'][np.argsort(-scores)]
-        top_items_without_labels = [np.argsort(-scores)]
+        print('prints the top 10 scores corresponding index for user %s' % user_id)
+        print(np.argsort(-scores)[:10].tolist())
 
-        print("User %s" % user_id)
+        # adds to a dictionary
+        # user_id is the dictionary key
+        # the index and the scores are in an array
 
-        print("     Recommended:")
+        dictionary[user_id]=[np.argsort(-scores)[:10].tolist(), np.sort(-scores[:10]).tolist()]
 
-        for x in top_items[:10]:
-            print("        %s" % x)
-
-        for x in top_items_with_labels[:10]:
-            print("        %s" % x)
-
-        for x in top_items_without_labels[:10]:
-            print("        %s" % x)
-
+    # prints the dictionary
+    print(dictionary)
+    # see if there is a difference when converted to json
+    print(json.dumps(dictionary))
+    # looks at the movie ids and scores for user_id=9
+    print(dictionary[9])
+    # same but in json to spot any differences
+    print(json.dumps(dictionary[9]))
 
 sample_output_to_visualisation(model, data)

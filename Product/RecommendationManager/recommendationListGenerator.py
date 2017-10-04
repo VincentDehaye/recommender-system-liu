457f7cc833d34d8b1a6ae026a84db7f00aee9d15
Marten Bolin <marbo914@student.liu.se>
2017-10-04 19:31:48 +0200

    Cleaned up the code and removed a lot of prints and no use comments.

--- Product/RecommendationManager/recommendationListGenerator.py
+++ Product/RecommendationManager/recommendationListGenerator.py
@@ -8,27 +8,8 @@ from Product.Database.DBConn import session, User, Movie, Rating
 from scipy.sparse import coo_matrix
 
 
-#This file in contrast to lightfm_example will try to use our own database to create a recommendation list.
-
-
-
-# Load the MovieLens 100k dataset.
-#data = fetch_movielens()
-
-#As we can see when printing, data['train'] is a "sparse matrix"
-#print('this is the train data in the dataset. (userid, movieid) rating')
-#print(data['train'])
-#As we can see when printing, data['test'] is a "sparse matrix"
-#print('this is the test data in the dataset')
-#print(data['test'])
-#As we can see when printing, data['item_labels'] is an array?,
-#print('this is the movies in the dataset')
-#print(data['item_labels'])
-
-print('-------------------------------')
-#print(data['item_features'])
-
-#TODO Save this information in the same format as data['train'] and data['test'], e.g. sparse matrix
+# This file in contrast to lightfm_example will try to use our own database to create a recommendation list.
+# Sets up all the lists that will be needed.
 Ratings = session.query(Rating).all()
 UserList = []
 MovieList = []
@@ -37,6 +18,9 @@ TestUserList = []
 TestMovieList = []
 TestRatingList = []
 
+# TODO Should only collect ratings over 3 or 5?
+# This way of doing a testmatrix is wrong it will result in a different size matrix then the Trainmatrix. We should load
+# the combination of every movie and user. If there is no rating by the user, this should be represented as a zero.
 for counter,row in enumerate(session.query(Rating.user_id,Rating.movie_id,Rating.rating)):
 
     if counter % 5 == 0:
@@ -48,23 +32,9 @@ for counter,row in enumerate(session.query(Rating.user_id,Rating.movie_id,Rating
         MovieList.append(row[1])
         RatingList.append(row[2])
 
+# Creates two sparse matrixes to make the data compatable with LightFM
 TrainMatrix = coo_matrix((RatingList,(UserList,MovieList)))
 TestMatrix = coo_matrix((TestRatingList,(TestUserList,TestMovieList)))
-print('                 this is OUR train data')
-print(TrainMatrix)
-print('                 this is OUR test data')
-print(TestMatrix)
-
-
-# Saves the data in the same format as the data[labels] list in lightfm example
-movieList=[]
-Movies = session.query(Movie).all()
-for row in session.query(Movie.title):
-    movieList.append(row[0])
-newMovieList=np.transpose(movieList)
-
-print('                 this is OUR labels')
-print(newMovieList)
 
 # Instantiate and train the model, change epochs to improve precision.
 model = LightFM(loss='warp')
@@ -72,45 +42,27 @@ model.fit(TrainMatrix, epochs=20, num_threads=2)
 
 # Evaluate the trained model by comparing it with the original data
 # It evaluates the precision for the top k=10 movies from the algorithm
-test_precision = precision_at_k(model, TrainMatrix, k=10).mean()
+# Could be a good idea to move this to another file since it takes some time to run
+test_precision = precision_at_k(model, TrainMatrix, k=5).mean()
+
 # this prints the test precision
 # the precision is in percentage.
 print('precision at train: %s' % test_precision)
 
 
 # Sends in our testmatrix and the user_ids for the users to present recommendations
-def sample_recommendation(model, trainmatrix, newMovieList, user_ids):
+def sample_recommendation(model, trainmatrix, user_ids):
     n_users, n_items = trainmatrix.shape
-    print("Our testmatrix shape: ")
-    print(trainmatrix.shape)
-    print("Our newMovieList shape: ")
-    print(newMovieList.shape)
     for user_id in user_ids:
-        # known_positives = newMovieList[testmatrix.tocsr()[user_id].indices]
 
         scores = model.predict(user_id, np.arange(n_items))
-        # print("This is the ng.arg")
-        # print(np.argsort(-scores))
         top_items = np.argsort(-scores)
         print("This is the recommended movie_ids for user:" + str(user_id))
-        for movie_id in top_items[:100]:
+        for movie_id in top_items[:10]:
             print(movie_id)
 
-        # top_items = newMovieList[np.argsort(-scores)]
 
-        # print("User %s" % user_id)
-        # print("     Known positives:")
-
-        # for x in known_positives[:3]:
-            # print("        %s" % x)
-
-        # print("     Recommended:")
-
-        # for x in top_items[:10]:
-        #    print("        %s" % x)
-
-
-# this method prints the recommended and known positives for the first three users
+# this method prints the recommended and known positives for the users in the defined range
 # known positives are movies that users have watched and rated highly
 # recommended are the movies that lightfm recommends.
 # observe that the user id is +1 and movie_id +1 in the dataset compared to the method output

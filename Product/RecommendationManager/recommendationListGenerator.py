216bb1a9cc5471040877a397317b74f46cfdaef5
Marten Bolin <marbo914@student.liu.se>
2017-10-11 11:49:00 +0200

    Commented the code

--- Product/RecommendationManager/recommendationListGenerator.py
+++ Product/RecommendationManager/recommendationListGenerator.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 from lightfm import LightFM
-#from lightfm.datasets import fetch_movielens
 from lightfm.evaluation import precision_at_k
 
 from Product.Database.DBConn import session, User, Movie, Rating
 from scipy.sparse import coo_matrix
 
-
 # This file in contrast to lightfm_example will try to use our own database to create a recommendation list.
+
 # Sets up all the lists that will be needed.
 Ratings = session.query(Rating).all()
 UserList = []
@@ -18,9 +17,11 @@ TestUserList = []
 TestMovieList = []
 TestRatingList = []
 
-# TODO Should only collect ratings over 3 or 5?
-# This way of doing a testmatrix is wrong it will result in a different size matrix then the Trainmatrix. We should load
-# the combination of every movie and user. If there is no rating by the user, this should be represented as a zero.
+# Filling the lists with data from the database
+# This way of doing a testmatrix is wrong it will result in a different size matrix then the Trainmatrix.
+# TODO Should only collect ratings over 3, otherwise even a one is treated as a positive rating. We should load
+# TODO the combination of every movie and user into the lists. If there is no rating by the user,
+# TODO this should be represented as a zero.
 for counter,row in enumerate(session.query(Rating.user_id,Rating.movie_id,Rating.rating)):
 
     if counter % 5 == 0:
@@ -32,21 +33,20 @@ for counter,row in enumerate(session.query(Rating.user_id,Rating.movie_id,Rating
         MovieList.append(row[1])
         RatingList.append(row[2])
 
-# Creates two sparse matrixes to make the data compatable with LightFM
+# Creates two sparse matrixes, to make the data compatable with LightFM
 TrainMatrix = coo_matrix((RatingList,(UserList,MovieList)))
 TestMatrix = coo_matrix((TestRatingList,(TestUserList,TestMovieList)))
 
-# Instantiate and train the model, change epochs to improve precision.
+# Instantiate and train the model, epochs is number of iterations of training done on the trainmatrix.
 model = LightFM(loss='warp')
 model.fit(TrainMatrix, epochs=20, num_threads=2)
 
 # Evaluate the trained model by comparing it with the original data
-# It evaluates the precision for the top k=10 movies from the algorithm
-# Could be a good idea to move this to another file since it takes some time to run
+# It evaluates the precision for the top k movies from the algorithm
+# Could be a good idea to move this to another file since it takes a lot of time to run
 test_precision = precision_at_k(model, TrainMatrix, k=5).mean()
 
 # this prints the test precision
-# the precision is in percentage.
 print('precision at train: %s' % test_precision)
 
 
@@ -61,11 +61,5 @@ def sample_recommendation(model, trainmatrix, user_ids):
         for movie_id in top_items[:10]:
             print(movie_id)
 
-
-# this method prints the recommended and known positives for the users in the defined range
-# known positives are movies that users have watched and rated highly
-# recommended are the movies that lightfm recommends.
-# observe that the user id is +1 and movie_id +1 in the dataset compared to the method output
-# That is because arrays start at 0 in python and.
-# TODO The output from this function should be a list of length 10 with ID:s that corresponds to the predicted movies.
+# Calls upon the samle_recommendation to create a recommendation list for user 56.
 sample_recommendation(model, TrainMatrix, range(56, 57))

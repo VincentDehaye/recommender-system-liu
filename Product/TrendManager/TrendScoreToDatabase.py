17147808705157bade1daa5aa5040629710c8553
John Lidquist <johli715@student.liu.se>
2017-10-12 14:23:04 +0200

    Continued work on TrendScoreToDatabase /johli715 & marbo914

--- Product/TrendManager/TrendScoreToDatabase.py
+++ Product/TrendManager/TrendScoreToDatabase.py
@@ -1,6 +1,16 @@
-from Product.TrendManager import TrendingController
-from Product.TrendManager import ScoredMovie
+from Product.TrendManager.TrendingController import TrendingController
+from Product.Database.DBConn import session
+from Product.Database.DBConn import Movie, MovieInGenre, Genre, TrendingScore
+
+# 1. Query movies from database
+# 2. Get new score for that movie
+# 3. If current trend score is different from the newly fetched score - Update score in database, else go to step 1
+# 4. Goto step 1
+
 
 trendController = TrendingController()
 scoredMovie = trendController.get_trending_content("pirates")
-print(scoredMovie.score)
+
+new = TrendingScore(movie_id=scoredMovie.id, total_score=scoredMovie.score, youtube_score=, twitter_score=)
+
+session.add()

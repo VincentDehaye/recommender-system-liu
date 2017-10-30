f0bdd79768a4746f26e41078f03ca50e323f3856
Marten Bolin <marbo914@student.liu.se>
2017-10-30 15:38:39 +0100

    Deleted prints and added in parameters to TrendScoreToDatabase. Also created a RunTrend file that starts the trending

--- Product/TrendManager/TrendScoreToDatabase.py
+++ Product/TrendManager/TrendScoreToDatabase.py
@@ -9,11 +9,12 @@ class TrendingToDB(object):
     # Call the trending to db to start filling the trend table in the database. This will be ran in the background
     # as long as the application is running
 
-    def __init__(self):
+    def __init__(self, background = True, continuous = True):
+        self.continous = continuous
         # creates the thread that will make the method run parallel. Sets daemon to true so that it will allow
         # the app to be terminated and will terminate with it
         thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True
+        thread.daemon = background
         thread.start()
 
     def run(self):
@@ -25,7 +26,28 @@ class TrendingToDB(object):
         # else go to step 1
         # 4. Go to step 1
         trend_controller = TrendingController()
-        while True:
+        res_movie = session.query(Movie).all()
+
+        for movie in res_movie:
+
+            res_score = session.query(TrendingScore).filter_by(movie_id=movie.id).first()
+
+            new_tot_score = trend_controller.get_trending_content(movie.title)  # gets new score
+
+            if res_score:
+                # if not the same
+                if new_tot_score != res_score.total_score:
+                    res_score.total_score = new_tot_score
+            else:
+                # if movie is not in trendingscore
+                movie = TrendingScore(movie_id=movie.id, total_score=new_tot_score, youtube_score=0,
+                                      twitter_score=0)
+                session.add(movie)
+            # The commit is in the loop for now due to high waiting time but could be moved outside to lower
+            # total run time
+            session.commit()
+
+        while self.continous:
 
             res_movie = session.query(Movie).all()
 
@@ -33,32 +55,18 @@ class TrendingToDB(object):
 
                 res_score = session.query(TrendingScore).filter_by(movie_id=movie.id).first()
 
-                # try first with set totscore and then comment it away and try with fetching totscore
-                # new_tot_score = 100
                 new_tot_score = trend_controller.get_trending_content(movie.title)  # gets new score
-                print("THis is movie id:")
                 print(movie.id)
 
                 if res_score:
                     if new_tot_score != res_score.total_score:
-                        print("NOT THE SAME SCORES - UPDATE")
+                        # If score is new
                         res_score.total_score = new_tot_score
-                    else:
-                        print("SAME SCORES - DO NOTHING")
                 else:
-                    print("No such id in TrendingScore table")
+                    # If movie is not in TrendingScore table
                     movie = TrendingScore(movie_id=movie.id, total_score=new_tot_score, youtube_score=0,
                                           twitter_score=0)
                     session.add(movie)
                 # The commit is in the loop for now due to high waiting time but could be moved outside to lower
                 # total run time
                 session.commit()
-
-# To test the code above see the code below. It will start the trendingtodb, and will then wait for 10 seconds
-# before first checkpoint and then wait another 10 before terminating. During this you can see that is db is
-# filled and then the method is terminated when the application is finished.
-trendingExample = TrendingToDB()
-time.sleep(10)
-print("Checkpoint")
-time.sleep(10)
-print("App is shutting down..")

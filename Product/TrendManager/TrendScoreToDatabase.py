7aa8408395da2d06a39b64571bd389b54389e890
Marten Bolin <marbo914@student.liu.se>
2017-10-30 10:57:22 +0100

    Deleted unnecessary code and moved commit into the loop / johli715 & marbo914

--- Product/TrendManager/TrendScoreToDatabase.py
+++ Product/TrendManager/TrendScoreToDatabase.py
@@ -50,8 +50,9 @@ class TrendingToDB(object):
                     movie = TrendingScore(movie_id=movie.id, total_score=new_tot_score, youtube_score=0,
                                           twitter_score=0)
                     session.add(movie)
-
-            session.commit()
+                # The commit is in the loop for now due to high waiting time but could be moved outside to lower
+                # total run time
+                session.commit()
 
 # To test the code above see the code below. It will start the trendingtodb, and will then wait for 10 seconds
 # before first checkpoint and then wait another 10 before terminating. During this you can see that is db is
@@ -61,36 +62,3 @@ time.sleep(10)
 print("Checkpoint")
 time.sleep(10)
 print("App is shutting down..")
-
-# This solution was only to show that it is the trendingcontroller that is the limiting part
-"""
-
-            for movie in resMovie:
-                in_db = False
-                # try first with set totscore and then comment it away and try with fetching totscore
-                # newTotScore = 100
-                newTotScore = trend_controller.get_trending_content(movie.title) #gets new score
-                print("THis is movie id:")
-                print(movie.id)
-
-                # Loop over each scoredMovie in resScore and see if movie is in it. if it is break check if it is a
-                # new total score
-                # or not and break the loop.
-                for scoredMovie in resScore:
-                    if scoredMovie.movie_id == movie.id:
-                        if newTotScore != scoredMovie.total_score:
-                            print("NOT THE SAME SCORES - UPDATE")
-                            scoredMovie.total_score = newTotScore
-                        else:
-                            print("SAME SCORES - DO NOTHING")
-                        in_db = True
-                        break
-
-                # If it goes through whole loop then we know it is a new movie and it can be put into the trending table
-                if not in_db:
-                    print("No such id in TrendingScore table")
-                    movie = TrendingScore(movie_id=movie.id, total_score=newTotScore, youtube_score=0, twitter_score=0)
-                    session.add(movie)
-
-            session.commit()
-"""

7822ae1245878e5ca7dd147b0eb88ac399490054
Erik Törngren <erito014@student.liu.se>
2017-10-31 12:46:58 +0100

    removed console.log

--- Product/Frontend/src/app/pages/authentication/_guards/auth.guard.ts
+++ Product/Frontend/src/app/pages/authentication/_guards/auth.guard.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 
-const output = console.log;
-
 @Injectable()
 export class AuthGuard implements CanActivate {
 
     constructor(private router: Router) { }
 
     canActivate() {
-      output('canActivate() is running');
         if (localStorage.getItem('currentUser')) {
-          output('True');
             // logged in so return true
             return true;
         }
-        output('False');
         // not logged in so redirect to login page
         this.router.navigate(['/pages/login']);
         return false;
--- Product/Frontend/src/app/pages/authentication/login/login.component.ts
+++ Product/Frontend/src/app/pages/authentication/login/login.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../_services/index';
 
-const output = console.log;
 
 @Component({
     moduleId: module.id,
@@ -25,15 +24,12 @@ export class LoginComponent implements OnInit {
     }
 
     login() {
-      output('login() is activated');
         this.loading = true;
         this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(result => {
                 if (result === true) {
-                  output('Correct');
                   this.router.navigate(['/']);
                 } else {
-                  output('Incorrect');
                     this.error = 'Username or password is incorrect';
                     this.loading = false;
                 }

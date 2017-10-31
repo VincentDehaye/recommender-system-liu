248a0feecdea674dd2f222d94cf503a16495fdbe
Alexander Dahl <aleda145@student.liu.se>
2017-10-31 16:47:13 +0100

    added logic how to get out an aggregated trending and user preferences score. Added TODOs. Move the generation to another file, movie the precision@k to another file. The code contains a lot of print statements at the moment but this will be changed and cleand up.

--- Product/RecommendationManager/recommendationListGenerator.py
+++ Product/RecommendationManager/recommendationListGenerator.py
@@ -3,7 +3,7 @@ import numpy as np
 from lightfm import LightFM
 from lightfm.evaluation import precision_at_k
 
-from Product.Database.DBConn import session, User, Movie, Rating
+from Product.Database.DBConn import session, User, Movie, Rating, TrendingScore
 from scipy.sparse import coo_matrix
 
 # This file in contrast to lightfm_example will try to use our own database to create a recommendation list.
@@ -33,33 +33,90 @@ for counter,row in enumerate(session.query(Rating.user_id,Rating.movie_id,Rating
         MovieList.append(row[1])
         RatingList.append(row[2])
 
+trending_scores={}
+for row in enumerate(session.query(TrendingScore.movie_id, TrendingScore.normalized_score)):
+    # row[1][0] is movie_id
+    # row[1][1] is normalized trending score
+    print(row[1][0])
+    trending_scores[row[1][0]]=row[1][1]
+
+print(trending_scores)
 # Creates two sparse matrixes, to make the data compatable with LightFM
 TrainMatrix = coo_matrix((RatingList,(UserList,MovieList)))
 TestMatrix = coo_matrix((TestRatingList,(TestUserList,TestMovieList)))
 
 # Instantiate and train the model, epochs is number of iterations of training done on the trainmatrix.
+# TODO save model to file after having been generated.
 model = LightFM(loss='warp')
 model.fit(TrainMatrix, epochs=20, num_threads=2)
 
 # Evaluate the trained model by comparing it with the original data
 # It evaluates the precision for the top k movies from the algorithm
 # Could be a good idea to move this to another file since it takes a lot of time to run
-test_precision = precision_at_k(model, TrainMatrix, k=5).mean()
+# TODO move this to another file.
+#test_precision = precision_at_k(model, TrainMatrix, k=5).mean()
 
 # this prints the test precision
-print('precision at train: %s' % test_precision)
+#print('precision at train: %s' % test_precision)
+def normalize_user_scores(scores):
+    min_score = np.amin(scores)
+    max_score = np.amax(scores)
 
+    for i in range(0, len(scores)):
+        scores[i] = (scores[i] - min_score)/(max_score-min_score)
+
+    return scores
 
 # Sends in our testmatrix and the user_ids for the users to present recommendations
 def sample_recommendation(model, trainmatrix, user_ids):
     n_users, n_items = trainmatrix.shape
     for user_id in user_ids:
-
+        movie_scores={}
         scores = model.predict(user_id, np.arange(n_items))
         top_items = np.argsort(-scores)
+        #movie_scores ={np.argsort(-scores) : scores}
+        #print(enumerate(np.argsort(-scores)))
+        #movie_scores ={enumerate(np.argsort(-scores)) : scores}
+#        movie_scores=[np.argsort(-scores)[:10].tolist(), np.sort(-scores[:10]).tolist()]
+      #  movie_scores={np.argsort(-scores).tolist() : scores.tolist()}
+        #movie_scores[np.argsort(-scores).tolist()] = scores.tolist()
+        #print('argsort')
+        #print(np.argsort(-scores).tolist())
+        #print('scores \n')
+        #print(scores.tolist())
+ #       for score in scores:
+#            print(score)
+        normalized_scores=normalize_user_scores(scores)
+        #print(movie_scores)
         print("This is the recommended movie_ids for user:" + str(user_id))
-        for movie_id in top_items[:10]:
-            print(movie_id)
+        #print(np.argsort(scores))
+
+        for movie_id in np.argsort(-scores):
+           # print(movie_id)
+            movie_scores[movie_id]=scores[movie_id]
+
+       # print(movie_scores)
+        #print(movie_scores[0])
+        trending_test={1: 0.5}
+        w = 1.5
+
+        print(movie_scores[1]+w*trending_test[1])
+        print(movie_scores[1]+w*trending_scores[1])
+        print(np.argsort(-scores)==np.argsort(-normalized_scores))
+        print(np.argsort(-scores))
+        print(np.argsort(-normalized_scores))
+
+        trending_and_user_pref_scores={}
+        for id in trending_scores:
+            print(id)
+            print(trending_scores[id])
+            print(movie_scores[id])
+            trending_and_user_pref_scores[id] = movie_scores[id]+w*trending_scores[id]
+            print(trending_and_user_pref_scores[id])
+
+            #print(scores[0])
+        #print(np.arange(n_items))
+
 
 # Calls upon the samle_recommendation to create a recommendation list for user 56.
 sample_recommendation(model, TrainMatrix, range(56, 57))

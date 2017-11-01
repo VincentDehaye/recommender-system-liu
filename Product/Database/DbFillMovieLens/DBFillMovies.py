fa04191cef0994260796b44079ca500c640799b4
Marten Bolin <marbo914@student.liu.se>
2017-11-01 14:53:13 +0100

    Moved DBConn back and added sys.path

--- Product/Database/DbFillMovieLens/DBFillMovies.py
+++ Product/Database/DbFillMovieLens/DBFillMovies.py
@@ -22,7 +22,7 @@ for genre in genreList:
 # This part handles adding the movies of the dataset into the database
 # Read the movie.csv file to add data into database
 # Columns in the ratings.csv: movieID, titleAndYear, Genres
-with open("./movies.csv", 'rt', encoding="utf-8") as f:
+with open(path, 'rt', encoding="utf-8") as f:
     reader = csv.reader(f)
 
     # Iterates through each row in the file and take column one (id) and column 2 (title)
@@ -45,7 +45,7 @@ with open("./movies.csv", 'rt', encoding="utf-8") as f:
     session.commit()
     f.close()
 
-with open('./movies.csv', 'rt', encoding="utf-8") as f:
+with open(path, 'rt', encoding="utf-8") as f:
     reader = csv.reader(f)
 
     for row in reader:

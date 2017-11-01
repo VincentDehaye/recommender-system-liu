a67f75773e5ec615a49562e376c811b1c8ca0788
Bamse <sebbe953@student.liu.se>
2017-11-01 14:45:17 +0100

    Serving static files from NGINX

--- Product/APIManager/main_app/urls.py
+++ Product/APIManager/main_app/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', serve, kwargs={'path': 'index.html'}),
     url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
-    RedirectView.as_view(url='/static/%(path)s', permanent=False)),
+        RedirectView.as_view(url='/static/%(path)s', permanent=False)),
     # url(r'^$', views.index, name='index'),
-    url(r'^api/v1/recommendations', views.recommendations, name='recommendations')
+    url(r'^v1/recommendations', views.recommendations, name='recommendations')
 ]

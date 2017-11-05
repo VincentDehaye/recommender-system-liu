866323b09f0f381ec62a8fec1f92441ee7ce1032
Alexander Dahl <aleda145@student.liu.se>
2017-11-05 17:45:55 +0100

    renamed CreateNewModel to create_new_model.py to follow PEP8

--- Product/RecommendationManager/createNewModel.py
+++
@@ -1,3 +0,0 @@
-import generateModel as gen_model
-
-gen_model.train_model('new_model.sav')
---
+++ Product/RecommendationManager/create_new_model.py
@@ -0,0 +1,7 @@
+"""
+This file creates a new model by calling the train model method
+in generate model. The param is the file name.
+"""
+from Product.RecommendationManager import generateModel as generate_model
+
+generate_model.train_model('new_model.sav')

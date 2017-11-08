b7ff4471a6434bcea8737d9c8f90827696d84c15
David schützer <davsc542@student.liu.se>
2017-11-08 16:28:57 +0100

    cleaned up in overview by removing unused components

--- Product/Frontend/src/app/pages/overview/overview.module.ts
+++ Product/Frontend/src/app/pages/overview/overview.module.ts
@@ -3,16 +3,7 @@ import { AngularEchartsModule } from 'ngx-echarts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { OverviewComponent } from './overview.component';
-import { StatusCardComponent } from './status-card/status-card.component';
-import { ActorsComponent } from './actors/actors.component';
-import { InfoComponent } from './info/info.component';
-import { ContentListsComponent } from './content-lists/content-lists.component';
-import { ViewsComponent } from './views/views.component';
-import { ViewsChartComponent } from './views/views-chart/views-chart.component';
-import { SubscribersComponent } from './subscribers/subscribers.component';
-import {RecommendationsPerformanceComponent} from './recommendations-performance/recommendations-performance.component';
-import { TrendingScoreComponent } from './trending-score/trending-score.component';
-import { TrendingScoreChartComponent } from './trending-score/trending-score-chart.component';
+
 
 
 @NgModule({
@@ -22,16 +13,7 @@ import { TrendingScoreChartComponent } from './trending-score/trending-score-cha
   ],
   declarations: [
     OverviewComponent,
-    StatusCardComponent,
-    ActorsComponent,
-    InfoComponent,
-    ContentListsComponent,
-    ViewsComponent,
-    ViewsChartComponent,
-    SubscribersComponent,
-    RecommendationsPerformanceComponent,
-    TrendingScoreComponent,
-    TrendingScoreChartComponent,
+
   ],
 })
 export class OverviewModule { }

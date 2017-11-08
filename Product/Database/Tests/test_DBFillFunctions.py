80989e8349b6b71010865cde027a2e91d325ce84
Marten Bolin <marbo914@student.liu.se>
2017-11-08 16:13:38 +0100

    Fixed test_DBFillFunctions to work with both large and small dataset

--- Product/Database/Tests/test_DBFillFunctions.py
+++ Product/Database/Tests/test_DBFillFunctions.py
@@ -7,13 +7,12 @@ from Product.Database.DBConn import session
 # entries in the corresponding csv files has been successfully entered into the db. We check this by doing queries
 # and asserting the result is the expected
 
+
 # Tests for DBFillUsers
 def test_DBFillUsers():
     result = session.query(User).filter_by(id=1).first()
     assert result.id == 1
 
-    result = session.query(User).filter_by(id=700).first()
-    assert result.id == 700
 
 # Tests for DBFillMovies
 def test_DBFillMovies():
@@ -23,10 +22,6 @@ def test_DBFillMovies():
     result = session.query(Movie).filter_by(id=1).first()
     assert result.title == "Toy Story"
     assert result.year == 1995
-
-    result = session.query(Movie).filter_by(id=164979).first()
-    print(result.title)
-    assert result.title == "Women of '69, Unboxed"
 
     result = session.query(MovieInGenre).filter_by(movie_id=1).all()
     for counter, res in enumerate(result):
@@ -42,26 +37,11 @@ def test_DBFillMovies():
             assert res.genre == "Fantasy"
 
 
-    result = session.query(MovieInGenre).filter_by(movie_id=164979).first()
-    assert result.genre == "Documentary"
-
 # Tests for DBFillRatings
 def test_DBFillRatings():
-    result = session.query(Rating).filter_by(user_id=1, movie_id=31).first()
-    assert result.rating == 2.5
+    result = session.query(Rating).filter_by(user_id=1, movie_id=13).first()
+    assert result.rating == 5.0
 
-    result = session.query(Rating).filter_by(user_id=671, movie_id=6565).first()
-    assert result.rating == 3.5
-
-# Tests for DBFillLinks
-def test_DBFillLinks():
-    result = session.query(MovieLinks).filter_by(movie_id=1).first()
-    assert result.imdb_id == 114709
-    assert result.tmdb_id == 862
-
-    result = session.query(MovieLinks).filter_by(movie_id=164979).first()
-    assert result.imdb_id == 3447228
-    assert result.tmdb_id == 410803
 
 
 

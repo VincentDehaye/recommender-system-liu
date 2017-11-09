ff3968a1e2240a2f720c734efa40911a79e92c58
Bamse <sebbe953@student.liu.se>
2017-11-09 21:40:19 +0100

    Changed API views to class based views
    Also made API browsable.

--- Product/APIManager/main_app/views.py
+++ Product/APIManager/main_app/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 def index(request):
     return HttpResponse("Hello, world!")
 
-def recommendations(request):
-    recs = {"movies":[
-        {"name":"Batman","id":1},
-        {"name":"Horseman","id":2},
-        {"name":"Birdperson","id":3},
-        {"name":"Manman","id":4},
-        {"name":"Cowman","id":5},
-        {"name":"Snakeman","id":6},
-        {"name":"Butterflyman","id":7},
-        {"name":"The extremely ordinary man","id":8},
-        {"name":"Wonderman the movie","id":9},
-        {"name":"Manbat","id":10},
-        ]}
-    #recs= {"name":"Batman","id":1}
-    return JsonResponse(recs)
+
+class RecommendationsView(APIView):
+
+    def get(self, request):
+        recs = {"movies":[
+            {"name":"Batman", "id":1},
+            {"name":"Horseman", "id":2},
+            {"name":"Birdperson", "id":3},
+            {"name":"Manman", "id":4},
+            {"name":"Cowman", "id":5},
+            {"name":"Snakeman", "id":6},
+            {"name":"Butterflyman", "id":7},
+            {"name":"The extremely ordinary man", "id":8},
+            {"name":"Wonderman the movie", "id":9},
+            {"name":"Manbat", "id":10},
+            ]}
+        #recs= {"name":"Batman","id":1}
+        return Response(recs)

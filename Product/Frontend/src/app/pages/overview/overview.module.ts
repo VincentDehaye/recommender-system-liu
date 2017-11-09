fff5e2696d7fcb988f821a9ba0340d4fb8d33be0
Anton Bergström <antbe230@liu.se>
2017-11-09 10:13:46 +0100

    added pie chart for overview and fixed trending according to mockup

--- Product/Frontend/src/app/pages/overview/overview.module.ts
+++ Product/Frontend/src/app/pages/overview/overview.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { AngularEchartsModule } from 'ngx-echarts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ChartModule } from 'angular2-chartjs';
+import { OverviewChartjsPieComponent } from './overview-components/rec-chartjs-pie.component';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { OverviewComponent } from './overview.component';
+import { OverviewRoutingModule, routedComponents } from './overview-routing.module';
 
 
+const components = [
+    OverviewChartjsPieComponent
+];
 
 @NgModule({
   imports: [
     ThemeModule,
     AngularEchartsModule,
+    NgxChartsModule, ChartModule,
+    OverviewRoutingModule,
   ],
   declarations: [
-    OverviewComponent,
+    OverviewComponent,...routedComponents, ...components,
 
   ],
 })
-export class OverviewModule { }
+export class OverviewModule {}

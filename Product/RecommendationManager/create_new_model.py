dee09de3e11731a62ac4352f492ed9db7ab6166d
Alexander Dahl <aleda145@student.liu.se>
2017-11-10 14:08:45 +0100

    updated docstrings in create_new_model.py

--- Product/RecommendationManager/create_new_model.py
+++ Product/RecommendationManager/create_new_model.py
@@ -1,6 +1,9 @@
-"""
-This file creates a new model by calling the train model method
-in generate model. The param is the file name.
+"""    Author: Alexander Dahl
+    Date: 2017-10-08
+    Last update: 2017-10-08
+    Purpose:
+    This file creates a new model by calling the train model method
+    in generate model. The param is the file name.
 """
 from Product.RecommendationManager import generate_model as generate_model
 

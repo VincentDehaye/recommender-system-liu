4fb7f1cd4e03a53c7b1d9534164f0bb55a610c93
Bamse <sebbe953@student.liu.se>
2017-11-11 20:32:22 +0100

    Fixed startup script to run NGINX backend and clean up on SIGINT

--- Product/start_frontend_development_servers.sh
+++ Product/start_frontend_development_servers.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-cd ../APIManager
-cd APIManager
-docker-compose up db &
+# Trap sigint (CTRL-C) and cleanup the development environment.
+trap "cleanup" INT
+
+cleanup() {
+	trap "" INT TERM
+	echo "**** Shutting down... ****"
+    docker-compose down
+	kill -TERM 0
+	wait
+	echo "Done with cleanup"
+}
+
+# Try to make sure that we're in the correct folder for running the script, then start the
+# development environment.
+cd Product
+cd ..
+cd Product
+docker-compose up &
 
 cd Frontend
-cd ../Frontend
 npm install
 npm start &
 
-
-cd ../APIManager
-docker-compose up web &
+# Wait for sigint
+cat

e12ff5114c96a317c33142a3b2e1f2dacaaa9e73
Bamse <sebbe953@student.liu.se>
2017-11-11 19:36:13 +0100

    Fixed docker startup crash
    Created a script to run all the startup commands for the gunicorn django
    server and making sure that the database exists before trying to connect
    to it so it doesn't crash.

---
+++ Product/django_docker_startup_command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+res=6
+while [ $res -ne 52 ]
+do
+	curl -s db:5432
+	res=$?
+done
+
+cd APIManager &&
+python manage.py makemigrations &&
+python manage.py migrate &&
+gunicorn project_config.wsgi --access-logfile '-' -b 0.0.0.0:8000

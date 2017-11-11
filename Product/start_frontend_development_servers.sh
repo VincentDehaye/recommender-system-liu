507c6352a5a87c953abfaf303343998606cd679d
Bamse <sebbe953@student.liu.se>
2017-11-11 20:49:34 +0100

    Prevent putting development in background from startup script
    This is because With startup in background the cleanup isn't performed.

--- Product/start_frontend_development_servers.sh
+++ Product/start_frontend_development_servers.sh
@@ -2,6 +2,15 @@
 
 # Trap sigint (CTRL-C) and cleanup the development environment.
 trap "cleanup" INT
+trap "echo tstp" TSTP
+
+echo "Putting this in background with CTRL-Z is not allowed because then you need to cleanup
+yourself. If you feel confident in doing that, you don't need this startup script! :P
+Stop and cleanup with CTRL-C"
+
+prevent() {
+	echo "Oopsie! Not allowed! Kill with CTRL-C"
+}
 
 cleanup() {
 	trap "" INT TERM

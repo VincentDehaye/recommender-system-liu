45ea81c89cea3aa7f1db4fbe58f567377a929bc4
Erik Törngren <erito014@student.liu.se>
2017-11-13 15:46:30 +0100

    Added a demographic box with age buttons and sex buttons

--- Product/Frontend/src/app/pages/users/users.component.ts
+++ Product/Frontend/src/app/pages/users/users.component.ts
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class UsersComponent implements OnInit {
   list: any;
 
+  paginationModel = 1;
+
+  iconToolbarModel = {
+    one: false,
+    two: false,
+  };
 
   ngOnInit() {
     this.list = ['Erik', 'Erik', 'Erik', 'Erik', 'Erik'];

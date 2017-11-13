b2a682f0dcc820815174e8ad7396b90d8d927750
John Lidquist <johli715@student.liu.se>
2017-11-13 11:57:59 +0100

    Added comments and changes according to pep8 in TrendScoreToDatabase. Also changed default parameters of it.

--- Product/TrendManager/TrendScoreToDatabase.py
+++ Product/TrendManager/TrendScoreToDatabase.py
@@ -9,14 +9,24 @@ from Product.TrendManager.TrendingController import TrendingController
 
 
 class TrendingToDB(object):
-    # Call the trending to db to start filling the trend table in the database. This will be ran in the background
-    # as long as the application is running
-
-    def __init__(self, background=True, continuous=True, daily=False):
-        # Background=True means that the application will be ran in daemon mode and other things can be ran
-        # simultaneously.
-        # continous=True means that the application will not be shut down after the first iteration
-        # daily=True means that it will be ran every 24h only works if cont is also True
+    """
+    Author: John Andree Lidquist, Marten Bolin
+    Date:
+    Last update: 13/11/2017
+    Purpose: This class handles collecting all the trending scores so that they can be stored in the database.
+    The class is using threads and will be abel to run in the background continuously
+    """
+    def __init__(self, background=False, continuous=False, daily=False):
+        """
+        Author: John Andree Lidquist, Marten Bolin
+        Date:
+        Last update:
+        Purpose: Instantiates the class, and based on the params an be run in different ways.
+        :param background: True - makes the process run on a thread in the background.
+        False - The process will not run on a thread.
+        :param daily: True - Will make the process run once every day.
+        False - Will only run the process once.
+        """
         self.continous = continuous
         self.stop = False
         self.daily = daily
@@ -38,8 +48,15 @@ class TrendingToDB(object):
             thread.start()
 
     def run(self):
-        # This is the actual method that will run until the application is shut down, it is done in the
-        # following steps
+        """
+        Author: John Andree Lidquist, Marten Bolin
+        Date:
+        Last update:
+        Purpose: The method where which will fetch all the scores by the trendingcontroller which communicate
+        with the Youtube and Twitter API.
+        """
+
+        # Fllowing steps are done:
         # 1. Query movies from database
         # 2. Get new score for that movie
         # 3. If current trend score is different from the newly fetched score - Update score in database,
@@ -70,10 +87,15 @@ class TrendingToDB(object):
                     if new_tot_score != res_score.total_score:
                         # If score is new
                         res_score.total_score = new_tot_score
-                        self.alter_trend.update_trend_score(movie_id=movie.id, total_score=new_tot_score, youtube_score=new_youtube_score, twitter_score=new_twitter_score)
+                        self.alter_trend.update_trend_score(movie_id=movie.id,
+                                                            total_score=new_tot_score,
+                                                            youtube_score=new_youtube_score,
+                                                            twitter_score=new_twitter_score)
                 else:
                     # If movie is not in TrendingScore table
-                    self.insert_trend.add_trend_score(movie_id=movie.id, total_score=new_tot_score, youtube_score=new_youtube_score,
+                    self.insert_trend.add_trend_score(movie_id=movie.id,
+                                                      total_score=new_tot_score,
+                                                      youtube_score=new_youtube_score,
                                                       twitter_score=new_twitter_score)
 
                 # The commit is in the loop for now due to high waiting time but could be moved outside to lower
@@ -84,7 +106,12 @@ class TrendingToDB(object):
 
         # Used to stop the thread if background is false or for any other reason it needs to be stopped.
     def terminate(self):
-        # Stops the scheduler
+        """
+        Author: John Andree Lidquist, Marten Bolin
+        Date:
+        Last update:
+        Purpose: Terminates the process
+        """
         self.stop = True
         if self.daily:
             self.scheduled.shutdown()

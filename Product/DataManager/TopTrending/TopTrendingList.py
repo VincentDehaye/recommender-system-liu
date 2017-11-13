d6ee7ccb8425598d45529c38935139a89d709d8c
Marten Bolin <marbo914@student.liu.se>
2017-11-13 09:26:13 +0100

    Added commenting to all files and also added a dict function in the TopTrendingList that returns a dict representation of the TopTrendingList

--- Product/DataManager/TopTrending/TopTrendingList.py
+++ Product/DataManager/TopTrending/TopTrendingList.py
@@ -1,8 +1,44 @@
+"""
+TopTrendingList Class
+"""
+
+
 class TopTrendingList(object):
+    """
+    Author: Marten Bolin
+    Date: 2017-11-10
+    Last update:
+    Purpose:
+    The list that will be sent to the APIManager
+    """
     def __init__(self, list_of_movies, list_of_scores):
-        self.list_of_movies=list_of_movies
-        self.list_of_scores=list_of_scores
+        """
+        Author: Marten Bolin
+        Date: 2017-11-10
+        Last update:
+        Purpose:
+        The constructor of the TopTrendingList, creates a movie list and a score list
+        """
+        self.list_of_movies = list_of_movies
+        self.list_of_scores = list_of_scores
 
     def print(self):
-        for (movie, score) in zip(self.list_of_movies,self.list_of_scores):
+        """
+        Author: Marten Bolin
+        Date: 2017-11-10
+        Last update:
+        Purpose:
+        Prints the list
+        """
+        for (movie, score) in zip(self.list_of_movies, self.list_of_scores):
             print("The movie", movie, "scored", score)
+
+    def dict(self):
+        """
+        Author: Marten Bolin
+        Date: 2017-11-10
+        Last update:
+        Purpose:
+        Returns the list as a dict
+        """
+        return dict(zip(self.list_of_movies, self.list_of_scores))

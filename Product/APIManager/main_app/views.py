5f60776b0c4f91779afa453cbc5105cfa955bc16
Bamse <sebbe953@student.liu.se>
2017-11-14 22:38:38 +0100

    Added docstrings according to Company standard

--- Product/APIManager/main_app/views.py
+++ Product/APIManager/main_app/views.py
@@ -1,46 +1,77 @@
 
 """
-This module contains classes that handle the API requests
+Author: Bamse
+Date: 2017-09-26
+Last update: 2017-11-14 by Bamse
+This module contains all the view classes that handle API requests.
 """
-from django.http import JsonResponse
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import traceback
 
 from main_app.serializers import RatingSerializer
 from Product.RecommendationManager.Recommendation.recommendation import Recommendation
 
 class RecommendationsView(APIView):
     """
-    This class is used to return the top 10 recommendations from Recommendations
+    Author: Bamse
+    Date: 2017-10-04
+    Last update: 2017-11-14 by Bamse
+    This class is used to return the top 10 recommendations from Recommendations team
     """
     # authentication_classes = (SessionAuthentication,)
     # permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         """
-        This function handles get requests to the server and returns a browsable API if gone
-        directly to in the browser.
+        Author: Bamse
+        Date: 2017-11-14
+        Last update: 2017-11-14 by Bamse
+        Purpose: Handles GET requests to recommendations API. Returns mock data if fetching from
+        recommendation doesn't work.
         """
-        recs = {"movies":[
-            {"name":"Batman", "id":1},
-            {"name":"Horseman", "id":2},
-            {"name":"Birdperson", "id":3},
-            {"name":"Manman", "id":4},
-            {"name":"Cowman", "id":5},
-            {"name":"Snakeman", "id":6},
-            {"name":"Butterflyman", "id":7},
-            {"name":"The extremely ordinary man", "id":8},
-            {"name":"Wonderman the movie", "id":9},
-            {"name":"Manbat", "id":10},
-            ]}
-        # recs = Recommendation(55, 10).generate_recommendation_list().__dict__
+        try:
+            recs = Recommendation(55, 10).generate_recommendation_list().__dict__
+        except ValueError:
+            recs = {"movies":[
+                {"name":"Batman", "id":1},
+                {"name":"Horseman", "id":2},
+                {"name":"Birdperson", "id":3},
+                {"name":"Manman", "id":4},
+                {"name":"Cowman", "id":5},
+                {"name":"Snakeman", "id":6},
+                {"name":"Butterflyman", "id":7},
+                {"name":"The extremely ordinary man", "id":8},
+                {"name":"Wonderman the movie", "id":9},
+                {"name":"Manbat", "id":10},
+                ]}
+        except:
+            traceback.print_exc()
+            recs = {"movies":[
+                {"name":"Batman", "id":1},
+                {"name":"Horseman", "id":2},
+                {"name":"Birdperson", "id":3},
+                {"name":"Manman", "id":4},
+                {"name":"Cowman", "id":5},
+                {"name":"Snakeman", "id":6},
+                {"name":"Butterflyman", "id":7},
+                {"name":"The extremely ordinary man", "id":8},
+                {"name":"Wonderman the movie", "id":9},
+                {"name":"Manbat", "id":10},
+                ]}
         return Response(recs)
 
 class TrendingView(APIView):
 
     def get(self, request):
+        """
+        Author: Bamse
+        Date: 2017-11-14
+        Last update: 2017-11-14 by Bamse
+        Purpose: Handles GET requests to trending API. Returns mock data.
+        """
         recs = {"trendingMovies":[
             {"name":"Batmantredning", "id":1},
             {"name":"Horseman", "id":2},

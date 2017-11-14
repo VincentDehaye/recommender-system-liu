380dbdf2f858a398b0ffae83abba8828b619b125
linnpetterssonn <linnlkp@telia.com>
2017-11-14 08:28:39 +0100

    Fixing code standards in RunTrend.py

--- Product/TrendManager/RunTrend.py
+++ Product/TrendManager/RunTrend.py
@@ -1,13 +1,19 @@
-from Product.TrendManager.TrendScoreToDatabase import TrendingToDB
+"""
+Running TrendManager to get trending score for movies in database
+"""
+
 import time
-# TrendingToDB has two in parameters, background means that it will be ran i the background until the application
-# terminates and continous = True means that it will run continously. If false it will only run one iteration
-# If background is set to True(default) a timer can be used in this case with time.sleep(seconds) that will make it
-# run for seconds
+
+from Product.TrendManager.TrendScoreToDatabase import TrendingToDB
+
+# TrendingToDB has two in parameters, background means that it will be run i the background
+# until the application terminates and continuous = True means that it will run continuously.
+# If false it will only run one iteration.
+# If background is set to True(default) a timer can be used in this case with time.sleep(seconds)
+# that will make it run for seconds
 # trending_run.terminate() will stop the function
 trending_run = TrendingToDB()
 time.sleep(50000)
 trending_run.terminate()
 time.sleep(10)
 print("Finished")
-

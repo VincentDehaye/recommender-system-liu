575b51cf7a743f477b11b8a901e9371c776152a2
Bamse <sebbe953@student.liu.se>
2017-11-14 16:37:17 +0100

    Changed Trending api to browsable

--- Product/APIManager/main_app/urls.py
+++ Product/APIManager/main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^api/v1/recommendations', views.RecommendationsView.as_view(), name='recommendations'),
     url(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v1/trending', views.trending, name='trending'),
+    url(r'^api/v1/trending', views.TrendingView.as_view(), name='trending'),
     url(r'^api/v1/youtubetrending', views.youtubetrending, name='youtubetrending'),
     url(r'^api/v1/twittertrending', views.twittertrending, name='twittertrending')
 ]
--- Product/APIManager/main_app/views.py
+++ Product/APIManager/main_app/views.py
@@ -21,41 +21,37 @@ class RecommendationsView(APIView):
         This function handles get requests to the server and returns a browsable API if gone
         directly to in the browser.
         """
-
+        recs = {"movies":[
+            {"name":"Batman", "id":1},
+            {"name":"Horseman", "id":2},
+            {"name":"Birdperson", "id":3},
+            {"name":"Manman", "id":4},
+            {"name":"Cowman", "id":5},
+            {"name":"Snakeman", "id":6},
+            {"name":"Butterflyman", "id":7},
+            {"name":"The extremely ordinary man", "id":8},
+            {"name":"Wonderman the movie", "id":9},
+            {"name":"Manbat", "id":10},
+            ]}
         recs = Recommendation(55, 10).generate_recommendation_list().__dict__
         return Response(recs)
 
-#def recommendations(request):
- #   recs = {"movies":[
-  #      {"name":"Batman","id":1},
-   #     {"name":"Horseman","id":2},
-    #    {"name":"Birdperson","id":3},
-     #   {"name":"Manman","id":4},
-      #  {"name":"Cowman","id":5},
-       # {"name":"Snakeman","id":6},
-        #{"name":"Butterflyman","id":7},
-        #{"name":"The extremely ordinary man","id":8},
-        #{"name":"Wonderman the movie","id":9},
-        #{"name":"Manbat","id":10},
-        #]}
-    #recs= {"name":"Batman","id":1}
-    #return JsonResponse(recs)
+class TrendingView(APIView):
 
-def trending(request):
-    recs = {"trendingMovies":[
-        {"name":"Batmantredning","id":1},
-        {"name":"Horseman","id":2},
-        {"name":"Birdperson","id":3},
-        {"name":"Manman","id":4},
-        {"name":"Cowman","id":5},
-        {"name":"Snakeman","id":6},
-        {"name":"Butterflyman","id":7},
-        {"name":"The extremely ordinary man","id":8},
-        {"name":"Wonderman the movie","id":9},
-        {"name":"Manbat","id":10},
-        ]}
-    #recs= {"name":"Batman","id":1}
-    return JsonResponse(recs)
+    def get(self, request):
+        recs = {"trendingMovies":[
+            {"name":"Batmantredning", "id":1},
+            {"name":"Horseman", "id":2},
+            {"name":"Birdperson", "id":3},
+            {"name":"Manman", "id":4},
+            {"name":"Cowman", "id":5},
+            {"name":"Snakeman", "id":6},
+            {"name":"Butterflyman", "id":7},
+            {"name":"The extremely ordinary man", "id":8},
+            {"name":"Wonderman the movie", "id":9},
+            {"name":"Manbat", "id":10},
+            ]}
+        return Response(recs)
 
 
 def youtubetrending(request):

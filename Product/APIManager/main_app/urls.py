a862485f501fa858c2793179d4e638657bb019e0
Bamse <sebbe953@student.liu.se>
2017-11-14 16:52:49 +0100

    Made all APIs browsable

--- Product/APIManager/main_app/urls.py
+++ Product/APIManager/main_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^api/v1/recommendations', views.RecommendationsView.as_view(), name='recommendations'),
     url(r'^api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/trending', views.TrendingView.as_view(), name='trending'),
-    url(r'^api/v1/youtubetrending', views.youtubetrending, name='youtubetrending'),
-    url(r'^api/v1/twittertrending', views.twittertrending, name='twittertrending')
+    url(r'^api/v1/youtubetrending', views.YoutubeTrendingView.as_view(), name='youtubetrending'),
+    url(r'^api/v1/twittertrending', views.TwitterTrendingView.as_view(), name='twittertrending')
 ]

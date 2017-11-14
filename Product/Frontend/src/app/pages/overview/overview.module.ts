f228768f548eb5b7ec75239a367ea214f66d7356
Elvin Granat <elvgr805@student.liu.se>
2017-11-14 21:49:21 +0100

    Fixed modularity of modals

--- Product/Frontend/src/app/pages/overview/overview.module.ts
+++ Product/Frontend/src/app/pages/overview/overview.module.ts
@@ -4,26 +4,21 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartModule } from 'angular2-chartjs';
 import { OverviewChartjsPieComponent } from './overview-components/rec-chartjs-pie.component';
 
-import { ModalsComponent } from '../components/modals/modals.component'
-import { ModalComponent } from '../components/modals/modal/modal.component'
-
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { OverviewComponent } from './overview.component';
 import { OverviewRoutingModule, routedComponents } from './overview-routing.module';
+import {ComponentsModule} from '../components/components.module';
 
 
 const components = [
     OverviewChartjsPieComponent,
-    ModalComponent,
-    ModalsComponent,
 ];
 @NgModule({
-  imports: [ThemeModule, AngularEchartsModule, NgxChartsModule, ChartModule, OverviewRoutingModule,
+  imports: [ComponentsModule, ThemeModule, AngularEchartsModule, NgxChartsModule, ChartModule, OverviewRoutingModule,
   ],
   declarations: [OverviewComponent , ...routedComponents, ...components],
   entryComponents: [
-    ModalComponent,
   ],
 })
 export class OverviewModule {}
--- Product/Frontend/src/app/pages/recommended/recommended.component.ts
+++ Product/Frontend/src/app/pages/recommended/recommended.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recommended.component.scss'],
   templateUrl: './recommended.component.html',
 })
-export class RecommendedComponent {}
+export class RecommendedComponent {
+  modalHeader1 = 'This is where you decide the modal header';
+  modalHeader2 = 'this is a different modal so it needs a different variable';
+  // Modal content 1-
+  modalContent1 = `this is the content that will be shown in the modal`;
+  modalContent2 = `same goes for this this is for the second modal`;
+}

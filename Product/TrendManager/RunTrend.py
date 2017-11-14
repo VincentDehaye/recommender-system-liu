c083c6f1303d2de6ea77063a36f407db1d1c86a7
Marten Bolin <marbo914@student.liu.se>
2017-11-14 14:53:06 +0100

    Improved comments in Runtrend explaining meaning of the in parameters

--- Product/TrendManager/RunTrend.py
+++ Product/TrendManager/RunTrend.py
@@ -1,11 +1,8 @@
 from Product.TrendManager.TrendScoreToDatabase import TrendingToDB
 import time
-# TrendingToDB has two in parameters, background means that it will be ran i the background until the application
-# terminates and continous = True means that it will run continously. If false it will only run one iteration
-# If background is set to True(default) a timer can be used in this case with time.sleep(seconds) that will make it
-# run for seconds
-# trending_run.terminate() will stop the function
+# TrendingToDB has two in parameters, daily which sets it to run once daily and daemon which if
+# True will make the TrendingToDB to terminate when the application is done
+# trending_run.terminate() will stop the TrendingToDB
 trending_run = TrendingToDB(daily=False)
 # time.sleep(150)
 # trending_run.terminate()
-

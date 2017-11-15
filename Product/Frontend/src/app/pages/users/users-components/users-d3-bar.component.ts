de1b2c5dde7843c8cbb518b17f9970147a906faa
Erik Törngren <erito014@student.liu.se>
2017-11-15 10:22:40 +0100

    pleased TSLint

--- Product/Frontend/src/app/pages/users/users-components/users-d3-bar.component.ts
+++ Product/Frontend/src/app/pages/users/users-components/users-d3-bar.component.ts
@@ -49,24 +49,24 @@ export class UsersD3BarComponent implements OnDestroy, OnInit {
   public getData() {
       this.dataHandlerService.getData().subscribe((data) => {
       this.movies = data.movies;
-      console.log();
-      console.log(this.movies[0]["name"]);
-      console.log(this.movies.values());
+      //console.log();
+      //console.log(this.movies[0]["name"]);
+      //console.log(this.movies.values());
      /* var i:number;
       for (i = 0;i < 9; ++i){
         this.results.push({name: this.movies[1]["name"], value: this.movies[1]['id']});
       }*/
       this.results = [
-        { name: this.movies[0]["name"], value: this.movies[0]['id']},
-        { name: this.movies[1]["name"], value: this.movies[1]['id']},
-        { name: this.movies[2]["name"], value: this.movies[2]['id']},
-        { name: this.movies[3]["name"], value: this.movies[3]['id']},
-        { name: this.movies[4]["name"], value: this.movies[4]['id']},
-        { name: this.movies[5]["name"], value: this.movies[5]['id']},
-        { name: this.movies[6]["name"], value: this.movies[6]['id']},
-        { name: this.movies[7]["name"], value: this.movies[7]['id']},
-        { name: this.movies[8]["name"], value: this.movies[8]['id']},
-        { name: this.movies[9]["name"], value: this.movies[9]['id']},
+        { name: this.movies[0]['name'], value: this.movies[0]['id']},
+        { name: this.movies[1]['name'], value: this.movies[1]['id']},
+        { name: this.movies[2]['name'], value: this.movies[2]['id']},
+        { name: this.movies[3]['name'], value: this.movies[3]['id']},
+        { name: this.movies[4]['name'], value: this.movies[4]['id']},
+        { name: this.movies[5]['name'], value: this.movies[5]['id']},
+        { name: this.movies[6]['name'], value: this.movies[6]['id']},
+        { name: this.movies[7]['name'], value: this.movies[7]['id']},
+        { name: this.movies[8]['name'], value: this.movies[8]['id']},
+        { name: this.movies[9]['name'], value: this.movies[9]['id']},
         ]
     }); // Converts the data making it reachable in the htm file
   }

11ff3d73ea262a4fb3b1d5e264dc115eb4cad45f
marbo914 <marbo914@student.liu.se>
2017-11-15 11:33:06 +0100

    Added trending to docker-compose file. Also added DBSmallFil lto RunTrend to make sure there is a database. Added check in filldatabase to see if it will go without crash due to unique.

--- Product/Database/DBFillSmallSet.py
+++ Product/Database/DBFillSmallSet.py
@@ -4,6 +4,7 @@ from Product.Database.DbFillMovieLens.DBFillMovies import FillMovies
 from Product.Database.DbFillMovieLens.DBFillRatings import FillRatings
 from Product.Database.DbFillMovieLens.DBFillUsers import FillUsers
 from Product.Database.DbFillMovieLens.DBFillLinks import FillLinks
+from Product.Database.DatabaseManager.Retrieve.RetrieveUser import RetrieveUser
 
 '''
 Author: John Andree Lidquist, Marten Bolin
@@ -20,10 +21,16 @@ Purpose: Fills the database with the small data set (40 movies) made for testing
 # The import is grayed out, but will run anyway.
 import Product.Database.DBConn
 
+retriever = RetrieveUser()
 # 2. Fill it with the data from the data set (by running DBFillUsers, DBFillMovies, DBFillRatings and DBFillLinks)
-useSmallDataSet = True
-u = FillUsers()
-f = FillMovies(useSmallDataSet)
-r = FillRatings(useSmallDataSet)
-print("No links added (not necessary)")
-# There are no Links created for the small data set
+if retriever.check_if_user_in_rating(1) is None:
+    useSmallDataSet = True
+    u = FillUsers()
+    print("Fyllt users en gang.")
+    f = FillMovies(useSmallDataSet)
+    r = FillRatings(useSmallDataSet)
+    print("No links added (not necessary)")
+    # There are no Links created for the small data set
+else:
+    print("There is already an existing database.")
+

b4bc605242ee37577238d16b990811d720ec581a
Alexander Dahl <aleda145@student.liu.se>
2017-11-15 14:47:34 +0100

    restored Recommendation class in DBConn and insert_recommendation_to_db will now populate all recommendations for all users.

--- Product/Database/DatabaseManager/Insert/InsertRecommendation.py
+++ Product/Database/DatabaseManager/Insert/InsertRecommendation.py
@@ -1,6 +1,6 @@
 from Product.Database.DatabaseManager.Insert.Insert import Insert
-from Product.Database.DBConn import Rating
-
+from Product.Database.DBConn import Recommendation
+from sqlalchemy import exists
 
 class InsertRecommendation(Insert):
     """
@@ -11,19 +11,13 @@ class InsertRecommendation(Insert):
     """
     def insert_recommendation(self, movie_list, user_id):
         """
-        Author: John Andree Lidquist
-        Date: 14/11/2017
-        Last update: 14/11/2017
+        Author: Alexander Dahl
+        Date: 2017-11-15
+        Last update: 2017-11-15
         Purpose: Make Inserts to the recommendation table in the database
         """
-        print(movie_list)
-        # TODO there is an error here when the corresponding user_id and movie_id already
-        # TODO exists in the database, how should that be fixed?
-        # TODO broken by Alexander Dahl
         for rec in movie_list:
-            print(rec['id'])
-            new_recommendation = Rating(movie_id=rec['id'], user_id=user_id, rating=None)
-        #print(new_recommendation)
+            new_recommendation = Recommendation(movie_id=rec['id'], user_id=user_id)
             self.session.add(new_recommendation)
             self.session.commit()
         self.session.close()

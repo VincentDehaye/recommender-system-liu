69b47f82c1c1b90378ed5183ac124c25c9d77aa7
Gustaf <gusno525@student.liu.se>
2017-11-15 11:57:18 +0100

    Created script showing model evolvement for the delivery meeting with Zenterio. (show_evolvement()) Also started function for showing model evolvement graphically (evolve_model_graph())

--- Product/RecommendationManager/tests/test_generate_model.py
+++ Product/RecommendationManager/tests/test_generate_model.py
@@ -46,4 +46,20 @@ def test_evolve_model():
     # since value_after > value_before
     assert(value_after - value_before >= 0)
 
+
+def test_test_precision():
+    """
+    Author: Alexander Dahl
+    Date: 2017-11-12
+    Last update: 2017-11-12
+    Purpose: Asserts that the lightfm precision@k puts out a positive number
+    """
+    # Pre-Conditions
+    generate_model.train_model('test_new_model.sav')
+    model = generate_model.load_model('test_new_model.sav')
+    train_matrix = get_matrices.get_train_matrix()
+    # Expected output
+    # >0
+    assert(generate_model.test_precision(model,train_matrix, 10) > 0)
+
 test_evolve_model()

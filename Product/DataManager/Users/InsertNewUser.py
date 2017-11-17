4771614ccdbf370b60ca7f12ea838cc8ca76b993
Marten Bolin <marbo914@student.liu.se>
2017-11-17 14:52:03 +0100

    Added InsertUser class to be able to insert new users from the DataManager folder

--- Product/DataManager/Users/InsertNewUser.py
+++ Product/DataManager/Users/InsertNewUser.py
@@ -1,16 +1,15 @@
-from Product.Database.DatabaseManager.Insert.Insert import Insert
-from Product.Database.DBConn import User
+from Product.Database.DatabaseManager.Insert.InsertUser import InsertUser
 
 
-class InsertUser(Insert):
+class InsertNewUser(object):
     """
     Author: Alexander Dahl, Marten Bolin
     Date: 2017-11-17
     Last update:
     Purpose: Make Insert of Feedback to the database
     """
-
-    def insert_user(self, age, gender, occupation=None):
+    @staticmethod
+    def insert_user(age, gender, occupation=None):
         """
         Author: Alexander Dahl, Marten Bolin
         Date: 2017-11-17
@@ -23,11 +22,4 @@ class InsertUser(Insert):
         :param occupation : The occupation of the user (Optional)
         :type String
         """
-        if occupation:
-            new_user = User(age=age, gender=gender, occupation=occupation)
-        else:
-            new_user = User(age=age, gender=gender, occupation=occupation)
-        self.session.add(new_user)
-        self.session.commit()
-        print('commited for user %s' % gender)
-        self.session.close()
+        InsertUser().insert_user(age=age, gender=gender,occupation=occupation)

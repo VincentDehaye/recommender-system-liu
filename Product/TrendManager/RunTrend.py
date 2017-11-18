8f296bae28c9c40b79bd4a75f49c1ac7e29f0a2e
linnpetterssonn <linnlkp@telia.com>
2017-11-18 15:31:17 +0100

    pep8 fixing in RunTrend.py

--- Product/TrendManager/RunTrend.py
+++ Product/TrendManager/RunTrend.py
@@ -1,12 +1,15 @@
 """
 Running TrendManager to get trending score for movies in database
 """
+
+# import time
+
 from Product.TrendManager.TrendScoreToDatabase import TrendingToDB
-import Product.Database.DBFillSmallSet
-import time
+# import Product.Database.DBFillSmallSet
+
 # TrendingToDB has two in parameters, daily which sets it to run once daily and daemon which if
 # True will make the TrendingToDB to terminate when the application is done
 # trending_run.terminate() will stop the TrendingToDB
-trending_run = TrendingToDB(daily=False)
+TRENDING_RUN = TrendingToDB(daily=False)
 # time.sleep(150)
 # trending_run.terminate()

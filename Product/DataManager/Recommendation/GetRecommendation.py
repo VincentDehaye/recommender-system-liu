affb27bbc11aa051d27c75d379348de99ad2f2ae
John Lidquist <johli715@student.liu.se>
2017-11-20 14:45:53 +0100

    Added retrieve_average_user_experience in DataManager

--- Product/DataManager/Recommendation/GetRecommendation.py
+++ Product/DataManager/Recommendation/GetRecommendation.py
@@ -9,6 +9,7 @@ class GetRecommendation:
     Purpose: Retrieve the number of recommended movies that have been watched, and the number that
     have not been watched
     """
+
     @staticmethod
     def retrieve_watched_and_not_watched():
         """
@@ -19,3 +20,14 @@ class GetRecommendation:
         have not been watched
         """
         return RetrieveRecommendation().retrieve_watched_and_not_watched()
+
+    @staticmethod
+    def retrieve_average_user_experience():
+        """
+        Author: John Andree Lidquist / Alexander Dahl
+        Date: 2017-11-20
+        Last update: 2017-11-20
+        Purpose: Retrieve the average number of recommended movies that have been watched, and the number that
+        have not been watched for each user
+        """
+        return RetrieveRecommendation().retrieve_average_user_experience()

493a9a29877b0c62a440e6aaa786e2d15d379135
John Lidquist <johli715@student.liu.se>
2017-11-20 11:32:34 +0100

    test_retrieve_trend_score method in test_RetrieveTrend finished and working

--- Product/Database/DatabaseManager/Tests/test_RetrieveTrending.py
+++ Product/Database/DatabaseManager/Tests/test_RetrieveTrending.py
@@ -1,17 +1,94 @@
-from Product.Database.DatabaseManager.Insert.InsertTrending import InsertTrending
+from Product.Database.DatabaseManager.Retrieve.RetrieveTrending import RetrieveTrending
 from Product.Database.DBConn import create_session
 from Product.Database.DBConn import TrendingScore
 from Product.Database.DBConn import Movie
 
-# TODO implement test functions
 
 def test_retrieve_trend_score():
     """
     Author: John Andrée Lidquist
     Date: 2017-11-16
+    Latest Update: 2017-11-20
     Purpose: Assert that trend total_score is retrieved from the database correctly
     """
-    assert True
+
+    # PRE-CONDITIONS
+    movie_id = -1
+    total_score = 100
+    youtube_score = 70
+    twitter_score = 30
+
+    # We create a session and add a dummy movie and a dummy total score
+    # Two commits are necessary because of foreign constraints
+    session = create_session()
+    dummy_movie = Movie(id=movie_id, title='Dummy', year=1111)
+    dummy_score = TrendingScore(movie_id=movie_id, total_score=total_score,
+                                youtube_score=youtube_score, twitter_score=twitter_score)
+    session.add(dummy_movie)
+    session.commit()
+    session.add(dummy_score)
+    session.commit()
+
+    # EXPECTED OUTPUT
+    expected_movie_id = movie_id
+    expected_total_score = total_score
+    expected_youtube_score = youtube_score
+    expected_twitter_score = twitter_score
+
+    # OBSERVED OUTPUT
+    # We call the method to be tested that retrieves all the users
+    observed_score = RetrieveTrending().retrieve_trend_score(movie_id=movie_id)
+    observed_score_five = RetrieveTrending().retrieve_trend_score(number_of_titles=5)
+    observed_score_all = RetrieveTrending().retrieve_trend_score()
+    observed_total_score = observed_score.total_score
+
+    observed_movie_id_1 = None
+    observed_total_score_1 = None
+    observed_youtube_score_1 = None
+    observed_twitter_score_1 = None
+    observed_movie_id_2 = None
+    observed_total_score_2 = None
+    observed_youtube_score_2 = None
+    observed_twitter_score_2 = None
+
+    for rating in observed_score_five:
+        if rating.movie_id == movie_id:
+            observed_movie_id_1 = rating.movie_id
+            observed_total_score_1 = rating.total_score
+            observed_youtube_score_1 = rating.youtube_score
+            observed_twitter_score_1 = rating.twitter_score
+            break
+
+    for rating in observed_score_all:
+        if rating.movie_id == movie_id:
+            observed_movie_id_2 = rating.movie_id
+            observed_total_score_2 = rating.total_score
+            observed_youtube_score_2 = rating.youtube_score
+            observed_twitter_score_2 = rating.twitter_score
+            break
+
+    # After adding the dummy user we remove them again.
+    # Two commits are necessary because of foreign constraints
+    session.delete(dummy_score)
+    session.commit()
+    session.delete(dummy_movie)
+    session.commit()
+    session.close()
+
+    assert observed_score
+    assert observed_total_score == expected_total_score
+
+    assert observed_score_five
+    assert observed_movie_id_1 == expected_movie_id
+    assert observed_total_score_1 == expected_total_score
+    assert observed_youtube_score_1 == expected_youtube_score
+    assert observed_twitter_score_1 == expected_twitter_score
+
+    assert observed_score_all
+    assert observed_movie_id_2 == expected_movie_id
+    assert observed_total_score_2 == expected_total_score
+    assert observed_youtube_score_2 == expected_youtube_score
+    assert observed_twitter_score_2 == expected_twitter_score
 
 
 def test_get_trending_twitter():

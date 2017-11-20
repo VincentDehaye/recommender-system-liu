818c2e21d29a7aa0d857477e97ad839649c0dd47
linnpetterssonn <linnlkp@telia.com>
2017-11-20 10:46:01 +0100

    Getting max result from twitter and youtube in db trending score

--- Product/Database/DatabaseManager/Retrieve/RetrieveTrending.py
+++ Product/Database/DatabaseManager/Retrieve/RetrieveTrending.py
@@ -2,6 +2,7 @@
 RetrieveTrending Class, is supposed to provide data from the Trending table in the database
 """
 from sqlalchemy import desc
+from sqlalchemy import func
 from Product.Database.DBConn import TrendingScore
 from Product.Database.DatabaseManager.Retrieve.Retrieve import Retrieve
 
@@ -54,4 +55,26 @@ class RetrieveTrending(Retrieve):
         :return TrendingScore : of type TrendingScore
         """
         query = self.session.query(TrendingScore).order_by(desc(TrendingScore.youtube_score)).limit(numOfTitles)
+        return query
+
+    def get_twitter_max(self):
+        """
+        Author: Linn Pettersson
+        Date: 2017-11-20
+        Last update:
+        Purpose: Get the highest stored Twitter score
+        :return: Query getting the max value from twitter_score in the database
+        """
+        query = self.session.query(func.max(TrendingScore.twitter_score))
+        return query
+
+    def get_youtube_max(self):
+        """
+        Author: Linn Pettersson
+        Date: 2017-11-20
+        Last update: 2017-11-20
+        Purpose: Get the highest stored Youtube score
+        :return: Query getting the max value from youtube_score in the database
+        """
+        query = self.session.query(func.max(TrendingScore.youtube_score))
         return query

1f11a7a5904418adf275f918e0bcfd6859e6ae5b
John Lidquist <johli715@student.liu.se>
2017-11-20 10:40:09 +0100

    test_retrieve_all_users method in test_RetrieveUser.py finished and working

--- Product/Database/DatabaseManager/Tests/test_RetrieveUser.py
+++ Product/Database/DatabaseManager/Tests/test_RetrieveUser.py
@@ -7,11 +7,15 @@ def test_check_if_user_in_rating():
     assert True
 
 
+def test_retrieve_largest_user_id():
+    assert True
+
+
 def test_retrieve_all_users():
     """
     Author: John Andrée Lidquist
     Date: 2017-11-16
-    Lates Update:
+    Lates Update: 2017-11-20
     Purpose: Assert that users are retrieved from the database correctly
     """
 
@@ -21,7 +25,7 @@ def test_retrieve_all_users():
     user_gender = "Male"
     user_occupation = "Student"
 
-    # We create a session and add a dummy movie to add a score to
+    # We create a session and add a dummy user
     session = create_session()
     dummy_user = User(id=user_id, age=user_age, gender=user_gender, occupation=user_occupation)
     session.add(dummy_user)
@@ -29,24 +33,36 @@ def test_retrieve_all_users():
 
     # EXPECTED OUTPUT
     expected_user_id = user_id
+    expected_user_age = user_age
+    expected_user_gender = user_gender
+    expected_user_occupation = user_occupation
 
     # OBSERVED OUTPUT
-    # We query the the score to get a observed output
+    # We call the method to be tested that retrieves all the users
     observed_all_users = RetrieveUser().retrieve_all_users()
 
-    observed_rating = 0
+    # We look for the user we added, and observe the attributes
+    observed_user_id = None
+    observed_user_age = None
+    observed_user_gender = None
+    observed_user_occupation = None
+
     for user in observed_all_users:
         if user.id == user_id:
-            observed_user = user.id
-            observed_age = user.id
-            observed_gender = user.id
-            observed_occupation = user.id
+            observed_user_id = user.id
+            observed_user_age = user.age
+            observed_user_gender = user.gender
+            observed_user_occupation = user.occupation
             break
 
-    # After adding the dummy movie and the dummy trending score for it, we remove them again.
-    # We need to commit twice because of foreign key constraints
+    # After adding the dummy userwe remove them again.
     session.delete(dummy_user)
     session.commit()
 
     assert observed_all_users
+    assert observed_user_id == expected_user_id
+    assert observed_user_age == expected_user_age
+    assert observed_user_gender == expected_user_gender
+    assert observed_user_occupation == expected_user_occupation
+
 

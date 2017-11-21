5c448db8bfc44a7a2dd7262af28d8aaf15acb048
John Lidquist <johli715@student.liu.se>
2017-11-21 17:56:06 +0100

    Finished test_InsertUser in the DatabaseManager

--- Product/Database/DatabaseManager/Tests/test_InsertUser.py
+++ Product/Database/DatabaseManager/Tests/test_InsertUser.py
@@ -1,9 +1,6 @@
-from Product.Database.DatabaseManager.Insert.InsertFeedback import InsertFeedback
+from Product.Database.DatabaseManager.Insert.InsertUser import InsertUser
 from Product.Database.DBConn import create_session
 from Product.Database.DBConn import User
-from Product.Database.DBConn import Movie
-from Product.Database.DBConn import Rating
-from Product.Database.DBConn import Recommendation
 
 
 def test_insert_user():
@@ -14,4 +11,48 @@ def test_insert_user():
     Purpose: Assert that users are correctly inserted to the database
     """
 
-    assert True
+    # PRE-CONDITIONS
+    user_age_1 = 3333
+    user_gender_1 = 'Female'
+    user_occupation_1 = 'Student'
+    user_age_2 = 4444
+    user_gender_2 = 'Male'
+
+    # We call the function to be tested and let it add feedback. Two users will be added
+    InsertUser().insert_user(user_age_1, user_gender_1, user_occupation_1)
+    InsertUser().insert_user(user_age_2, user_gender_2)
+
+    # EXPECTED OUTPUT
+    expected_user_age_1 = user_age_1
+    expected_user_gender_1 = user_gender_1
+    expected_user_occupation_1 = user_occupation_1
+    expected_user_age_2 = user_age_2
+    expected_user_gender_2 = user_gender_2
+    expected_user_occupation_2 = 'Unknown'
+
+    # OBSERVED OUTPUT
+    # We query the rating and recommendation to get an observed output
+    session = create_session()
+    observed_1 = session.query(User).filter(User.age == user_age_1).first()
+    observed_user_age_1 = observed_1.age
+    observed_user_gender_1 = observed_1.gender
+    observed_user_occupation_1 = observed_1.occupation
+    observed_2 = session.query(User).filter(User.age == user_age_2).first()
+    observed_user_age_2 = observed_2.age
+    observed_user_gender_2 = observed_2.gender
+    observed_user_occupation_2 = observed_2.occupation
+
+    # After adding the dummy users, we remove them again.
+    session.delete(observed_1)
+    session.delete(observed_2)
+    session.commit()
+    session.close()
+
+    assert observed_1
+    assert observed_user_age_1 == expected_user_age_1
+    assert observed_user_gender_1 == expected_user_gender_1
+    assert observed_user_occupation_1 == expected_user_occupation_1
+    assert observed_2
+    assert observed_user_age_2 == expected_user_age_2
+    assert observed_user_gender_2 == expected_user_gender_2
+    assert observed_user_occupation_2 == expected_user_occupation_2

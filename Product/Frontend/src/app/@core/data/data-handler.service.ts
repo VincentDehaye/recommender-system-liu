1039c67f18e29c8d0ef741191940e76f09aa6f7a
Bamse <sebbe953@student.liu.se>
2017-11-22 16:48:42 +0100

    Consistent use of male as recommendation query parameter
    Change one instance of man to male.

--- Product/Frontend/src/app/@core/data/data-handler.service.ts
+++ Product/Frontend/src/app/@core/data/data-handler.service.ts
@@ -36,7 +36,7 @@ readonly ROOT_URLtwitter = this.apiUrl + '/v1/twittertrending';
   getMetaRecommendationsData(age_lower = 0, age_upper = 200, male = true, female = true, other = true): any {
     let filter: string = '';
     filter += 'age_lower=' + age_lower.toString() + '&age_upper' + age_upper.toString();
-    if (male) { filter += '&man=1'; }
+    if (male) { filter += '&male=1'; }
     if (other) {filter += '&other=1'; }
     if (female) {filter += '&female=1'; }
 

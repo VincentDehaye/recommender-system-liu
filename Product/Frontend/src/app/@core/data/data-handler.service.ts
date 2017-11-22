bdb7d26486d33e5c7da73df97ad535ca9ae0b871
Bamse <sebbe953@student.liu.se>
2017-11-22 16:57:33 +0100

    Fixed error in query URL for recommendations

--- Product/Frontend/src/app/@core/data/data-handler.service.ts
+++ Product/Frontend/src/app/@core/data/data-handler.service.ts
@@ -35,7 +35,7 @@ readonly ROOT_URLtwitter = this.apiUrl + '/v1/twittertrending';
   }
   getMetaRecommendationsData(age_lower = 0, age_upper = 200, male = true, female = true, other = true): any {
     let filter: string = '';
-    filter += 'age_lower=' + age_lower.toString() + '&age_upper' + age_upper.toString();
+    filter += 'age_lower=' + age_lower.toString() + '&age_upper=' + age_upper.toString();
     if (male) { filter += '&male=1'; }
     if (other) {filter += '&other=1'; }
     if (female) {filter += '&female=1'; }

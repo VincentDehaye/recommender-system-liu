a1b51f3cee3f5c20e0819bd9348a7e45ce6cf520
John Lidquist <johli715@student.liu.se>
2017-11-23 16:49:07 +0100

    Updated docstring of GetSuccessRate

--- Product/DataManager/Recommendation/GetSuccessRate.py
+++ Product/DataManager/Recommendation/GetSuccessRate.py
@@ -1,13 +1,15 @@
+"""
+Retrieves two different Success Rate from the database
+"""
 from Product.Database.DatabaseManager.Retrieve.RetrieveSuccessRate import RetrieveSuccessRate
 
 
 class GetSuccessRate:
     """
-    Author: John Andree Lidquist / Alexander Dahl
+    Author: John Andree Lidquist, David Cerny
     Date: 2017-11-20
-    Last update: 2017-11-20
-    Purpose: Retrieve the number of recommended movies that have been watched, and the number that
-    have not been watched
+    Last update: 2017-11-23
+    Purpose: Retrieves two different Success Rate from the database
     """
 
     @staticmethod
@@ -15,11 +17,11 @@ class GetSuccessRate:
         """
         Author: John Andree Lidquist, David Cerny
         Date: 2017-11-20
-        Last update: 2017-11-20
-        Purpose: Retrieve the number of recommended movies that have been watched, and the number that
-        have not been watched
+        Last update: 2017-11-23
+        Purpose: Retrieve the values watched, not_watched and the timestamp from the table
+        SuccessRate from the database and returns them in a list of dictionaries.
+        :return List of dictionaries with keys watched, not_watched and timestamp
         """
-        # TODO docstring needs updating.
         success_rate_dict = []
         all_rates = RetrieveSuccessRate().get_simple_success_rate()
         for rate in all_rates:
@@ -32,15 +34,15 @@ class GetSuccessRate:
         """
         Author: John Andree Lidquist, David Cerny
         Date: 2017-11-20
-        Last update: 2017-11-20
-        Purpose: Retrieve the average number of recommended movies that have been watched, and the number that
-        have not been watched for each user
+        Last update: 2017-11-23
+        Purpose: Retrieve the value average_user_success_rate and timestamp from the table
+        SuccessRate from the database and returns them in a list of dictionaries.
+        :return List of dictionaries with keys average_user_success_rate and timestamp
         """
-        # TODO update docstring
+
         result = []
         all_rates = RetrieveSuccessRate().get_average_user_success_rate()
         for rate in all_rates:
             result.append({'average_user_success_rate': rate.average_user_success_rate,
                            'timestamp': rate.timestamp})
         return result
-

73b8b615269017bc59fb52d058b478c617048c15
Marten Bolin <marbo914@student.liu.se>
2017-11-23 19:21:43 +0100

    Added doc strings and adjusted to PEP

--- Product/RecommendationManager/Recommendation/create_recommendations_for_all_users.py
+++ Product/RecommendationManager/Recommendation/create_recommendations_for_all_users.py
@@ -1,23 +1,36 @@
 """
-    Author: Alexander Dahl
+    Author: Alexander Dahl, Marten Bolin
     Date: 2017-11-15
-    Last update: 2017-11-15
+    Last update: 2017-11-23
     Purpose:
-    This module populates the database with recommendations for all users.
+    This module populates the database with recommendations for users.
 """
 from Product.Database.DatabaseManager.Retrieve.RetrieveUser import RetrieveUser
 from Product.RecommendationManager.Recommendation.recommendation import Recommendation
 
 
 class CreateRecommendationsForAllUsers:
-
+    """
+    Author: Alexander Dahl, Marten Bolin
+    Date: 2017-11-15
+    Last update: 2017-11-15
+    Purpose:
+    This module populates the database with recommendations for all users
+    """
     @staticmethod
     def execute(number_of_users=None):
-        USERS = RetrieveUser().retrieve_all_users()
+        """
+        Author: Alexander Dahl, Marten Bolin
+        Date: 2017-11-15
+        Last update: 2017-11-13
+        Purpose:
+        This fucntion populates the database with recommendations for all users.
+        :param number_of_users : how many users to create recommendations for
+        """
+        users = RetrieveUser().retrieve_all_users()
         if not number_of_users:
-            number_of_users=len(USERS)
+            number_of_users = len(users)
         # populates the database with all the recommendations for all users
-        for user,user_number in zip(USERS,range(0,number_of_users)):
+        for user, user_number in zip(users, range(0, number_of_users)):
             # the Recommendation class will insert it to the database when it is generated
             Recommendation(user.id, 10).generate_recommendation_list()
-

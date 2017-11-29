4b13ee04d771b8a603630944e302b67d8f46df7b
Ariyan Abdulla <ariab702@student.liu.se>
2017-11-29 13:58:36 +0100

    Corrected the infobuttons on the different pages which describes the graphs.

--- Product/Frontend/src/app/pages/overview/overview.component.ts
+++ Product/Frontend/src/app/pages/overview/overview.component.ts
@@ -30,7 +30,9 @@ export class OverviewComponent implements OnInit {
   modalHeader4 = 'List for the top trending content';
   modalContent1 = `This graph shows the number of times a movie has been recommended in total from Coogl3's algorithm.`;
   modalContent2 = `This list shows the top recommended movies and their title.
-The first movie in the list is the movie that has been recommended the most.`;
+The first movie in the list is the movie that has been recommended the most.
+The percentage value shows the rate of success for each movie,
+meaning how many times a movie has been watched when recommended`;
   modalContent3 = `This graph shows the top trending movies and the score produced from Coogl3's algorithm.`;
   modalContent4 = `This list shows the top trending movies and their title.
 The first movie in the list is the one with the highest score.`;

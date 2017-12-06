d289d13f3b6ebf5a6281f0a6beb2e4074d71999f
Bamse <sebbe953@student.liu.se>
2017-12-06 17:33:56 +0100

    Returned to non reload gunicorn since shouldn't be used in production

--- Docker/django_docker_startup_command.sh
+++ Docker/django_docker_startup_command.sh
@@ -22,7 +22,7 @@ echo "yes" | python manage.py collectstatic &&
 python manage.py create_user &&
 
 if [ ${PRODUCTION:-0} -eq 1 ]; then
-	gunicorn project_config.wsgi --reload --access-logfile '-' -b 0.0.0.0:8000
+	gunicorn project_config.wsgi --access-logfile '-' -b 0.0.0.0:8000
 else
 	gunicorn project_config.wsgi --reload --access-logfile '-' -b 0.0.0.0:8000
 fi
